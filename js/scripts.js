37d64d78e22f0e05187d549ba53119a46f62d342
lukelash <63415678+lukelash@users.noreply.github.com>
2022-06-28 17:22:53 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // const navbar = document.body.querySelector('#sideNav');
+    // navbar.setAttribute("style", "width:30vw");
+    // const resume_content = document.body.querySelector("#resume-content");
+    // resume_content.setAttribute("style", "width:70vw")
+
+    // document.title = "COCKS";
+    // document.body.setAttribute("style", "font-size:3vw")
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
